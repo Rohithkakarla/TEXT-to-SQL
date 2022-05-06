7fc5c095e8f912f023ee284863172db3bc14add8
RohithKakarla <karthik.kakarla30@gmail.com>
2022-05-06 22:52:15 +0530

    spider dataset training

--- test_sparc.sh
+++ test_sparc.sh
@@ -7,7 +7,7 @@
 # 2. train and evaluate.
 #    the result (models, logs, prediction outputs) are saved in $LOGDIR
 
-GLOVE_PATH="/kaggle/working/glove.840B.300d.txt" # you need to change this
+GLOVE_PATH="/kaggle/working/glove.840B.300d.txt"
 LOGDIR="logs/logs_sparc"
 
 CUDA_VISIBLE_DEVICES=0 python3 run.py --raw_train_filename="data/sparc_data_removefrom/train.pkl" \
